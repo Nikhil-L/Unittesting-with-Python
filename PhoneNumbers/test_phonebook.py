d07ee3992b9aa8fe444f0f57b64bbef4837d0112
Nikhil-L <nikhil89515@gmail.com>
2021-05-25 19:38:08 +0530

    update

---
+++ PhoneNumbers/test_phonebook.py
@@ -0,0 +1,28 @@
+
+import unittest
+from phonebook import PhoneBook
+
+class PhoneBookTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.phonebook = PhoneBook()
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_lookup_by_name(self):
+        self.phonebook.add("Bob", "12345")
+        number = self.phonebook.lookup("Bob")
+        self.assertEqual("12345", number)
+
+    def test_missing_name(self):
+        with self.assertRaises(KeyError):
+            self.phonebook.lookup("missing")
+
+    @unittest.skip("WIP")
+    def test_empty_phonebook_is_consistent(self):
+        self.assertTrue(self.phonebook.is_consistent)
+
+
+if __name__ == '__main__':
+    unittest.main()
